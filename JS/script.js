961550f5cc8d341dba1646c702cf58c724333e8e
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-18 17:39:06 +0100

    feat: 10 api calls completed

--- JS/script.js
+++ JS/script.js
@@ -9,8 +9,23 @@
 // Il metodo .then server per controllare la risposta situata nel Parametro d'ingresso della call back function
 // Dentro alla call back function andiamo ad eseguire il codice quando l'api ottiene risposta
 // Il parametro response serve per accedere all'oggetto JSON risultato dalla nostra chiamata
-axios
-  .get("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then((response) => {
-    console.log(response.data.response);
-  });
+
+const ulElement = document.querySelector("ul");
+// console.log(ulElement);
+
+// Eseguiamo 10 volte la chiamata API
+for (let i = 1; i <= 10; i++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => {
+      //   console.log(response.data.response);
+
+      // Facciamo elemento html lista e gli passiamo una proprieta dell'oggetto ottenuto tramite API JSON
+      const listaHTML = `<li>${response.data.response}</li>`;
+
+      // Aggiungiamo questo elemento lista all'elemento ul selezionato prima
+      ulElement.innerHTML += listaHTML;
+    });
+
+  // Finisce il ciclo e riparte una nuova iterazione quindi una nuova chiamata API
+}
